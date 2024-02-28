9cfb5e69f854ed1df53a195f9e39e0e2e4fd9ef4
JiangTianJason <38585628+JiangTianJason@users.noreply.github.com>
2024-02-28 10:42:11 +0800

    Section 5.3.2, Table 4

---
+++ label_misprediction.py
@@ -0,0 +1,21 @@
+########################Top-3 Label Misprediction in Table 4#######################
+
+
+from collections import Counter
+
+def misprediction_top_3(victim_model,attack_method):
+    all_indices = [eval(line) for line in open(r"./evaluation_result/{}/{}.txt".format(victim_model, attack_method), 'r',
+                                               encoding="utf-8").readlines()]
+
+    all_count = []
+    for i in all_indices:
+        for j in i["label_switching"]:
+            origin_mis = "".join(j)
+            all_count.append(origin_mis)
+
+    d2 = Counter(all_count)
+    sorted_x = sorted(d2.items(), key=lambda x: x[1], reverse=True)
+    print(sorted_x)
+
+if __name__ == "__main__":
+    misprediction_top_3("xlm","textfooler")  ###xlm,bert,albert
